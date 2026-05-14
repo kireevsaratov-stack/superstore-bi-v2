9fa4253137f9cb8f94a6f9c2c75b13012067b945
Roman <roman@MacBook-Air-Roman.local>
2026-05-14 21:25:47 +0300

    fix captions and spacing

--- app.py
+++ app.py
@@ -311,13 +311,14 @@ with col2:
         st.markdown('##### Топ-10 убыточных продуктов')
         l10 = df.groupby('Product Name')['Profit'].sum().nsmallest(10).reset_index()
         l10['Product Name'] = l10['Product Name'].apply(lambda x: x[:30] + '...' if len(x) > 30 else x)
-        fig = px.bar(l10, x='Profit', y='Product Name', orientation='h', template=plotly_template,
+        fig = px.bar(l10, y='Profit', x='Product Name', orientation='v', template=plotly_template,
                      color_discrete_sequence=['#EF553B'] * 10,
                      labels={'Profit': 'Прибыль', 'Product Name': ''})
-        fig.update_traces(text=l10['Profit'].apply(lambda x: f'-{format_k(abs(x), currency)}'), textposition='outside', textfont=dict(size=11))
+        fig.update_traces(text=l10['Profit'].apply(lambda x: f'-{format_k(abs(x), currency)}'), textposition='outside',
+                          textfont=dict(size=11))
         fig.update_layout(
-            yaxis={'categoryorder': 'total descending', 'automargin': True, 'tickfont': dict(size=10)},
-            xaxis=dict(range=[l10['Profit'].min() * 1.15, 0], tickmode='array',
+            xaxis={'categoryorder': 'total descending', 'tickfont': dict(size=10)},
+            yaxis=dict(range=[l10['Profit'].min() * 1.15, 0], tickmode='array',
                        tickvals=[-8000, -6000, -4000, -2000, 0], ticktext=['-8K', '-6K', '-4K', '-2K', '0']),
             coloraxis_showscale=False,
             height=400,
